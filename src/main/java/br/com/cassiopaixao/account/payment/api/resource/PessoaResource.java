1320255791a3a17058e32fc441fd37c62609acc3
Cássio Paixão <cassiopaixaoti@gmail.com>
2017-11-21 00:51:31 -0200

    functionality update and delete pessoa

--- src/main/java/br/com/cassiopaixao/account/payment/api/resource/PessoaResource.java
+++ src/main/java/br/com/cassiopaixao/account/payment/api/resource/PessoaResource.java
@@ -3,20 +3,25 @@ package br.com.cassiopaixao.account.payment.api.resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cassiopaixao.account.payment.api.event.RecursoCriadoEvent;
 import br.com.cassiopaixao.account.payment.api.model.Pessoa;
 import br.com.cassiopaixao.account.payment.api.repository.PessoaRepository;
+import br.com.cassiopaixao.account.payment.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -27,6 +32,9 @@ public class PessoaResource {
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
@@ -41,4 +49,15 @@ public class PessoaResource {
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
 	}
 
+	@DeleteMapping("{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletePessoa(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
+	}
+	
+	@PutMapping("{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
+	}
 }
