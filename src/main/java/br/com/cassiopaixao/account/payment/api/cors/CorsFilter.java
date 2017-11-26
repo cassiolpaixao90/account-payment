f4e5ae381c551196314b644760bc7f598270ce01
Cássio Paixão <cassiopaixaoti@gmail.com>
2017-11-26 10:36:17 -0200

    add cors

---
+++ src/main/java/br/com/cassiopaixao/account/payment/api/cors/CorsFilter.java
@@ -0,0 +1,53 @@
+package br.com.cassiopaixao.account.payment.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.crsh.shell.impl.command.system.help;
+
+public class CorsFilter implements Filter {
+	
+	private String originPermitida = "";
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		
+		response.setHeader("Acess-Control-Allow-Origin", originPermitida);
+		response.setHeader("Acess-Control-Allow-Credentials", "true");
+		
+		if("OPTION".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+			response.setHeader("Acess-Control-Allow-Methods", "POST, GET, DELETE, PUT,OPTIONS");
+			response.setHeader("Acess-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			response.setHeader("Acess-Control-Allow-Max-Age", "3600");
+			response.setStatus(HttpServletResponse.SC_OK);
+		}else {
+			chain.doFilter(request, response);
+		}
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
